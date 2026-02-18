3b5b52a77358c24932a269852644d9e6dad569d5
anerudh21 <anerudh21@gmail.com>
2026-02-18 14:35:57 +0530

    Coded to print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * This class demonstrates a simple Java program that uses
+ * the datatype String and prints it.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+class OOPSBannerApp {
+    public static void main(String[] args) {
+        String s = "OOPS";
+        System.out.println(s);
+    }
+}
